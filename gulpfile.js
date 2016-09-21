3c56448c3529a68fe325c8b5a14994970fd0d67e
Leeje408 <leeje408@gmail.com>
2016-09-21 12:16:08 +0800

    webpack + gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 // / <reference path="../node/node.d.ts" />
 var gulp = require('gulp')
+var webpack = require('webpack')
 gulp.task('default', function () {
-    // gulp default todo
+  // gulp default todo
+  // gulp.start('webpack')
+})
+
+gulp.task('webpack', function () {
+  // webpack todo
+  var config = {
+    entry: {
+      index: ''
+    }
+  }
+  webpack(config, function () {})
 })
